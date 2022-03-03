4738c838cfd2afbe9ada0cb02bdb93fb93c623fe
Mahmoud Bashir <anadeouf@yahoo.com>
2022-03-03 23:27:08 +0200

    Mahmoud , work on splashScreenActivity , changing on Themes .

--- app/src/main/java/com/mahmoudbashir/onyxdelivery/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/mahmoudbashir/onyxdelivery/ui/activities/SplashScreenActivity.kt
@@ -1,14 +1,16 @@
 package com.mahmoudbashir.onyxdelivery.ui.activities
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mahmoudbashir.onyxdelivery.R
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class SplashScreenActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() , CoroutineScope{
+    override val coroutineContext: CoroutineContext get() = Dispatchers.Main + Job()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -18,7 +20,8 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private fun navigateToMainActivity() {
-        GlobalScope.launch {
+
+        launch {
             delay(2000)
             Intent(this@SplashScreenActivity,MainActivity::class.java).also {
                 startActivity(it)
