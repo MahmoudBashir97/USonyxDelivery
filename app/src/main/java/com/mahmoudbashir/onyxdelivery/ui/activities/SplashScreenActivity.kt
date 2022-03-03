46b8fedca614c76548a53c6f7773c14626c797e7
Mahmoud Bashir <anadeouf@yahoo.com>
2022-03-03 23:08:29 +0200

    Mahmoud , work on splashScreenActivity , adding some drawables.

--- app/src/main/java/com/mahmoudbashir/onyxdelivery/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/mahmoudbashir/onyxdelivery/ui/activities/SplashScreenActivity.kt
@@ -1,12 +1,29 @@
 package com.mahmoudbashir.onyxdelivery.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mahmoudbashir.onyxdelivery.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // here we navigate to MainActivity after some seconds
+        navigateToMainActivity()
+    }
+
+    private fun navigateToMainActivity() {
+        GlobalScope.launch {
+            delay(2000)
+            Intent(this@SplashScreenActivity,MainActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
     }
 }
