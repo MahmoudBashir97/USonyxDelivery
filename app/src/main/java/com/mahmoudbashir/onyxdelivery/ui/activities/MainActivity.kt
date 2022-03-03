8b8b7706812aff817266ff2fb3b862714fa07ce2
Mahmoud Bashir <anadeouf@yahoo.com>
2022-03-04 01:25:24 +0200

    Mahmoud ,work on LoginScreenFragment , add transition for login form inputs with show more btn.

--- app/src/main/java/com/mahmoudbashir/onyxdelivery/ui/activities/MainActivity.kt
+++ app/src/main/java/com/mahmoudbashir/onyxdelivery/ui/activities/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.mahmoudbashir.onyxdelivery.ui.activities
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import com.mahmoudbashir.onyxdelivery.R
 import com.mahmoudbashir.onyxdelivery.databinding.ActivityMainBinding
 
@@ -10,8 +15,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(mainBinding.root)
+        window.statusBarColor = ContextCompat.getColor(this,R.color.inputsColor)
 
+        window?.decorView?.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
--- app/src/main/java/com/mahmoudbashir/onyxdelivery/ui/fragments/LoginScreenFragment.kt
+++ app/src/main/java/com/mahmoudbashir/onyxdelivery/ui/fragments/LoginScreenFragment.kt
@@ -1,21 +1,56 @@
 package com.mahmoudbashir.onyxdelivery.ui.fragments
 
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.mahmoudbashir.onyxdelivery.R
+import com.mahmoudbashir.onyxdelivery.databinding.FragmentLoginScreenBinding
+import kotlin.math.log
 
 
 class LoginScreenFragment : Fragment() {
-
+    lateinit var loginBinding : FragmentLoginScreenBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_screen, container, false)
+        loginBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_login_screen, container, false)
+
+
+        return loginBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+       doTransitionWithShiwMoreLessBtn()
+    }
+
+    private fun doTransitionWithShiwMoreLessBtn() {
+        loginBinding.apply {
+            showMoreLessBtn.setOnClickListener {
+                if (hiddenView.visibility == View.VISIBLE){
+
+                    TransitionManager.beginDelayedTransition(bigLin,
+                        AutoTransition()
+                    )
+                    hiddenView.visibility = View.GONE
+                    showMoreLessBtn.text = "Show Less"
+                }else{
+                    TransitionManager.beginDelayedTransition(bigLin,
+                        AutoTransition()
+                    )
+                    hiddenView.visibility = View.VISIBLE
+                    showMoreLessBtn.text = "Show More"
+                }
+            }
+        }
     }
 
 
