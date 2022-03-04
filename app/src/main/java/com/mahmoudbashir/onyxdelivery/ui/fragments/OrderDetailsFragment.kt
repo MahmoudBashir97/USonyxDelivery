bb04c36329edea48c9a6fa6bebf5fbc24409f712
Mahmoud Bashir <anadeouf@yahoo.com>
2022-03-04 23:12:44 +0200

    Mahmoud , work on OrderDetailsFragment ,

--- app/src/main/java/com/mahmoudbashir/onyxdelivery/ui/fragments/OrderDetailsFragment.kt
+++ app/src/main/java/com/mahmoudbashir/onyxdelivery/ui/fragments/OrderDetailsFragment.kt
@@ -5,17 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.mahmoudbashir.onyxdelivery.R
+import com.mahmoudbashir.onyxdelivery.databinding.FragmentOrderDetailsBinding
 
 
 class OrderDetailsFragment : Fragment() {
+
+    lateinit var detailsBinding: FragmentOrderDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_details, container, false)
+        detailsBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_order_details, container, false)
+
+        return detailsBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        backToPrevScreen()
+    }
+
+    private fun backToPrevScreen() {
+        detailsBinding.apply {
+            backBtn.setOnClickListener {
+                findNavController().navigateUp()
+            }
+        }
     }
 
 }
