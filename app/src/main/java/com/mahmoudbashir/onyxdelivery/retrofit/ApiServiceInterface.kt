56deca0da30164a4e49c0940e29d98001ea16fdd
Mahmoud Bashir <anadeouf@yahoo.com>
2022-03-04 13:27:47 +0200

    Mahmoud , add Constants class for initializing BASE_URL , add ApiServiceInterface and RetrofitInstance .

---
+++ app/src/main/java/com/mahmoudbashir/onyxdelivery/retrofit/ApiServiceInterface.kt
@@ -0,0 +1,4 @@
+package com.mahmoudbashir.onyxdelivery.retrofit
+
+interface ApiServiceInterface {
+}
---
+++ app/src/main/java/com/mahmoudbashir/onyxdelivery/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.mahmoudbashir.onyxdelivery.utils
+
+object Constants {
+
+    const val BASE_URL = "http://mapp.yemensoft.net/"
+}
